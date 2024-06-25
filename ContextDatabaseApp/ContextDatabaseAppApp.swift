d885101b63b130724f5a3d335c5f06ef69f1fde7
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2024-06-26 04:01:53 +0900

    Initial Commit

---
+++ ContextDatabaseApp/ContextDatabaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextDatabaseAppApp.swift
+//  ContextDatabaseApp
+//
+//  Created by 高橋直希 on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ContextDatabaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
