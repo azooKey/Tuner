16fe56da10dd783345f8e6262ad08c4e290dc665
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2024-07-05 15:38:13 +0900

    数字と記号列の時は保存しない

--- ContextDatabaseApp/Model/TextModel.swift
+++ ContextDatabaseApp/Model/TextModel.swift
@@ -145,6 +145,11 @@ class TextModel: ObservableObject {
                 return
             }
 
+            // 記号か数字のみのテキストはスキップ
+            if cleanedText.utf16.isSymbolOrNumber{
+                return
+            }
+
             // 最後のテキストの前方一致文字列であればやめる
             let lastText = texts.last?.text.utf16 ?? "".utf16
             if cleanedText.utf16.starts(with: lastText) && texts.count > 0 {
@@ -394,6 +399,11 @@ class TextModel: ObservableObject {
         var duplicatedCount = 0
 
         for entry in entries {
+            // 記号のみのエントリは削除
+            if entry.text.utf16.isSymbolOrNumber {
+               continue
+            }
+
             // 除外アプリの場合はスキップ
             if avoidApps.contains(entry.appName) || minTextLength > entry.text.utf8.count {
                 continue
@@ -445,5 +455,24 @@ extension Character {
 
     var isNumber: Bool {
         return ("0"..."9").contains(self)
+    }
+}
+
+extension String.UTF16View {
+    // 記号か数字のみか判定
+    var isSymbolOrNumber: Bool {
+        for unit in self {
+            switch unit {
+            case 0x0030...0x0039, // 数字 '0' (0x0030) から '9' (0x0039)
+                 0x0021...0x002F, // 記号 '!' (0x0021) から '/'
+                 0x003A...0x0040, // 記号 ':' (0x003A) から '@'
+                 0x005B...0x0060, // 記号 '[' (0x005B) から '`'
+                 0x007B...0x007E: // 記号 '{' (0x007B) から '~'
+                continue
+            default:
+                return false
+            }
+        }
+        return true
     }
 }
