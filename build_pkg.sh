48292bf986b868b3126441c71875e32f1876c0a3
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2025-04-03 23:51:30 +0900

    fix build

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# エラー時に即座に終了
+set -e
+
 # バージョン設定
 VERSION="1.0.0"
 APP_NAME="Tuner"
@@ -21,20 +24,32 @@ xcodebuild -project Tuner.xcodeproj -scheme Tuner -configuration Release CODE_SI
 DERIVED_DATA_APP="$HOME/Library/Developer/Xcode/DerivedData/Tuner-ciqmcchfmnbfecalwkclsxhusmkq/Build/Products/Release/$APP_NAME.app"
 cp -R "$DERIVED_DATA_APP" "$APPLICATIONS_DIR/"
 
-# pkgbuildの実行
+# パッケージの分析
+pkgbuild --analyze --root "$TEMP_DIR" pkg.plist
+
+# 一時パッケージの作成
 pkgbuild --root "$TEMP_DIR" \
+         --component-plist pkg.plist \
          --identifier "$BUNDLE_ID" \
          --version "$VERSION" \
          --install-location "/Applications" \
-         "$PKG_DIR/$APP_NAME.pkg"
+         "$PKG_DIR/$APP_NAME-tmp.pkg"
 
-# productbuildの実行
+# 最終パッケージの作成
 productbuild --distribution Distribution.xml \
              --package-path "$PKG_DIR" \
              --version "$VERSION" \
              "$BUILD_DIR/$APP_NAME-$VERSION.pkg"
 
+# 署名
+productsign --sign "Developer ID Installer" \
+            "$BUILD_DIR/$APP_NAME-$VERSION.pkg" \
+            "$BUILD_DIR/$APP_NAME-$VERSION-signed.pkg"
+
+# 署名済みパッケージを元の名前に移動
+mv "$BUILD_DIR/$APP_NAME-$VERSION-signed.pkg" "$BUILD_DIR/$APP_NAME-$VERSION.pkg"
+
 # クリーンアップ
-rm -rf "$TEMP_DIR" "$PKG_DIR"
+rm -rf "$TEMP_DIR" "$PKG_DIR" pkg.plist
 
 echo "PKGファイルの作成が完了しました: $BUILD_DIR/$APP_NAME-$VERSION.pkg" 
